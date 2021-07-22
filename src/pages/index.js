83890031643ea4f2f1af5a52ff5822d7edcad76b
Aaron Wilson <aaronwilson1234@outlook.ie>
2021-07-22 14:34:23 +0100

    adding widget for viewing resume, futher css needed

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,6 +17,7 @@ export default () => {
         <meta name="description" content={description || 'Gatsby Simplefolio'} />
       </Helmet>
       <App />
+      
     </>
   );
 };
---
+++ src/pages/resume.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function resume() {
+    return (
+        <div styles={{
+   display: "flex",
+   justifyContent:"center",
+   alignItems:"center",
+   width:"100%"
+}}>
+        
+            
+<div class="elfsight-app-b4c6f310-7db2-4076-a589-45264e86470d"></div>
+        </div>
+    )
+}
